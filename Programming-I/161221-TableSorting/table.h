2507d8007a5df055571a407968ada2d6895b1058
YanB25 <442826556@qq.com>
2016-12-26 21:57:12 +0800

    Update table.h

--- Programming-I/161221-TableSorting/table.h
+++ Programming-I/161221-TableSorting/table.h
@@ -1,37 +1,18 @@
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef _TABLE_H_
+#define _TABLE_H_
 
-struct tabletype{
-	char gender;
+typedef struct Stu
+{
+	char gender[2];
 	char name[7];
-	int id, score;
-};
+	int id;
+	int score;
+}Stu;
 
-typedef struct tabletype tb;
+void print(Stu* stus, int n);
+int GenderCmp(const void*, const void*);
+int NameCmp(const void*, const void*);
+int IdCmp(const void*, const void*);
+int ScoreCmp(const void*, const void*);
 
-//Private
-int cmp_gender(const tb * const , const tb * const );
-
-int cmp_name(const tb * const , const tb * const );
-
-int cmp_id(const tb * const , const tb * const );
-
-int cmp_score(const tb * const , const tb * const );
-
-void myqsort(tb ** , int ,int ,int (*cmp)(const tb * const, const tb * const));
-
-//Public
-void read(tb * );
-
-void print(tb ** , int );
-
-void sort_by_gender(tb ** , int );
-
-void sort_by_name(tb ** , int );
-
-void sort_by_id(tb ** , int );
-
-void sort_by_score(tb ** , int );
-
-void disp(tb ** , int );
+#endif
