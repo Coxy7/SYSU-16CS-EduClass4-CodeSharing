ff316a129c025391f96d0d7e0c58dbe5e8b1c3d2
YanB25 <442826556@qq.com>
2016-12-26 21:53:49 +0800

    Update main.c

--- Programming-I/161221-TableSorting/main.c
+++ Programming-I/161221-TableSorting/main.c
@@ -1,38 +1,30 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include "table.h"
-int main(){
-	int size, i;
-	tb ** ori, ** cur;
-	scanf("%d", &size);
-	ori = (tb **)malloc( sizeof(tb *) * size );
-	cur = (tb **)malloc( sizeof(tb *) * size );
-
-	for ( i = 0; i < size; i++ ){
-		ori[i] = malloc( sizeof(tb) );
-		read( ori[i] );
-		cur[i] = malloc( sizeof(tb) );
-		*cur[i] = *ori[i];
+int main()
+{
+	//freopen("input","r",stdin);
+	int n;
+	scanf("%d",&n);
+	Stu *stus = (Stu*)malloc(n * sizeof(Stu));
+	Stu *sort_stus = (Stu*)malloc(n * sizeof(Stu));
+	for (int i = 0; i < n; ++i)
+	{
+		scanf("%s%s%d%d", stus[i].gender, stus[i].name, &stus[i].id, &stus[i].score);
+		sort_stus[i] = stus[i];
 	}
-
-	while ( scanf("%d",&i) == 1 && i != -1 ){
-		if ( i == 0 )
-			print( ori , size );
-		else if ( i == 1 )
-			sort_by_gender( cur , size );
-		else if ( i == 2 )
-			sort_by_name( cur , size );
-		else if ( i == 3 )
-			sort_by_id( cur , size );
-		else if ( i == 4 )
-			sort_by_score( cur , size );
-
-		if ( i ) print( cur , size );
+	int op;
+	while(scanf("%d",&op) == 1)
+	{
+		if (op == -1) break;
+		if (op == 0) { print(stus,n); continue; }
+		if (op == 1) qsort(sort_stus, n, sizeof(*stus), GenderCmp);
+		if (op == 2) qsort(sort_stus, n, sizeof(*stus), NameCmp);
+		if (op == 3) qsort(sort_stus, n, sizeof(*stus), IdCmp);
+		if (op == 4) qsort(sort_stus, n, sizeof(*stus), ScoreCmp);
+		print(sort_stus, n);
 	}
-
-	disp( ori , size );
-	disp( cur , size );
-
-	return 0;
+  free(stus);
+  free(sort_stus);
 }
